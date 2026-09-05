74ec3fab6c5b8773c2ec1fb8fb78149bac3a213e
Sanjit Basker <tijnasreksab@gmail.com>
2026-09-05 14:32:42 -0400

    clone ben davis extensions

--- index.ts
+++ index.ts
@@ -2,10 +2,15 @@ import type { ExtensionAPI } from "@earendil-works/pi-coding-agent";
 import userPermissions from "./01-user-permissions/index.ts";
 import descriptiveFooter from "./02-descriptive-footer/index.ts";
 import observationalMemory from "./03-observational-memory/index.ts";
-import clangd from "./04-clangd/index.ts";
+import backgroundTerminals from "./background-terminals/index.ts";
+import fileSearch from "./file-search/index.ts";
+import subagents from "./subagents/index.ts";
 
 export default function piExtensions(pi: ExtensionAPI) {
   userPermissions(pi);
   descriptiveFooter(pi);
   observationalMemory(pi);
+  backgroundTerminals(pi);
+  fileSearch(pi);
+  subagents(pi);
 }
